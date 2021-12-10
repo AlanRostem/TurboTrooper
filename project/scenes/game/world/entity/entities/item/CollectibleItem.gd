a824c9bd3194498e65c57e950575a53d19faf352
Alan Rostem <arostem@gmail.com>
2021-12-10 15:39:04 +0100

    new new

--- project/scenes/game/world/entity/entities/item/CollectibleItem.gd
+++ project/scenes/game/world/entity/entities/item/CollectibleItem.gd
@@ -7,15 +7,28 @@ var __flashing = false
 
 var __player_found = null
 
+var _collected = false
+
+export var __disappear_on_pickup = true
+export var __disappear_on_timeout = true
+
+onready var __life_timer = $LifeTimer
+
+func _ready():
+	if __disappear_on_timeout:
+		__life_timer.start()
+
 func _process(delta):
-	if _life_timer.time_left < _life_timer.wait_time / 2 and !__flashing:
+	if __disappear_on_timeout and _life_timer.time_left < _life_timer.wait_time / 2 and !__flashing:
 		__flash_timer.start()
 		__flashing = true
 		
 	if __player_found != null:
-		if _pick_up_condition(__player_found):
+		if _pick_up_condition(__player_found) and !_collected:
+			_collected = true
 			_player_collected(__player_found)
-			queue_free()
+			if __disappear_on_pickup:
+				queue_free()
 		
 func _physics_process(delta):
 	if is_on_floor():
---
+++ project/scenes/game/world/entity/entities/item/collectible_items/objective/ObjectiveItem.gd
@@ -0,0 +1,26 @@
+extends "res://scenes/game/world/entity/entities/item/CollectibleItem.gd"
+
+const MAX_FOLLOW_SPEED = 140
+const FOLLOW_LERP_WEIGHT = .10
+const MIN_FOLLOW_X_DIST = 16
+
+onready var __body_shape = $BodyShape
+
+func _physics_process(delta):
+	if !_collected: return
+	var dir = parent_world.player_node.position - position
+	var vel = dir.normalized() * MAX_FOLLOW_SPEED
+	var prev = get_velocity()
+	if abs(dir.x) > MIN_FOLLOW_X_DIST:
+		set_velocity_x(lerp(prev.x, vel.x, FOLLOW_LERP_WEIGHT))
+	elif abs(dir.x) < MIN_FOLLOW_X_DIST - 2:
+		set_velocity_x(lerp(prev.x, 0, FOLLOW_LERP_WEIGHT))
+	set_velocity_y(lerp(prev.y, vel.y, FOLLOW_LERP_WEIGHT))
+
+func _player_collected(player):
+	set_attached_to_player(true)
+		
+func set_attached_to_player(value):
+	_collected = value
+	__body_shape.disabled = value
+	is_gravity_enabled = !value
