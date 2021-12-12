eed194d5e43fbd1c08c015ed83da54e5f11d719e
Alan Rostem <arostem@gmail.com>
2021-12-12 20:43:20 +0100

    dust trail added

--- project/scenes/game/world/entity/entities/player/components/state_machine/PlayerRunState.gd
+++ project/scenes/game/world/entity/entities/player/components/state_machine/PlayerRunState.gd
@@ -1,9 +1,23 @@
 extends "res://scenes/game/world/entity/entities/player/components/state_machine/PlayerState.gd"
 
+var __dust_trail_scene = preload("res://scenes/game/world/other/DustTrailEffect.tscn")
+
+onready var __dust_spawn_timer = $DustSpawnTimer
+
+var __spawning_dust = false
+
 func movement_update(delta):
 	if player.is_moving_too_fast(player.max_dash_speed):
 		parent_state_machine.transition_to("PlayerSlideState")
 		return
+	
+	if player.is_on_ground() and player.has_max_dash_charge():
+		if !__spawning_dust:
+			__dust_spawn_timer.start()
+			__spawning_dust = true
+	elif __spawning_dust:
+		__spawning_dust = false
+		__dust_spawn_timer.stop()
 	
 	if (move_left or move_right) and player.can_move_on_ground():
 		var dir = int(move_right) - int(move_left)
@@ -33,3 +47,10 @@ func movement_update(delta):
 			player.clear_dash_charge()
 		elif !slide_by_fire_button:
 			parent_state_machine.transition_to("PlayerSlideState")
+
+func exit():
+	__spawning_dust = false
+	__dust_spawn_timer.stop()
+
+func _on_DustSpawnTimer_timeout():
+	player.parent_world.show_effect_deferred(__dust_trail_scene, player.position)
--- project/scenes/game/world/other/EffectSprite.gd
+++ project/scenes/game/world/other/EffectSprite.gd
@@ -1,4 +1,8 @@
 extends AnimatedSprite
 
+func _ready():
+	frame = 0
+	play("default")
+
 func _on_EffectSprite_animation_finished():
 	queue_free()
