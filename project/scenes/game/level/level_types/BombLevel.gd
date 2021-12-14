d9fd2d37733f8df95fbaf8e42d19a5f585283d99
Alan Rostem <arostem@gmail.com>
2021-12-14 13:27:28 +0100

    bomb switch

---
+++ project/scenes/game/level/level_types/BombLevel.gd
@@ -0,0 +1,12 @@
+extends "res://scenes/game/level/Level.gd"
+
+
+onready var __bomb_switch = $GameWorld/BombSwitch
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ project/scenes/game/world/objective/BombSwitch.gd
@@ -0,0 +1,30 @@
+extends "res://scenes/game/world/objective/Objective.gd"
+
+export var __detonation_time = 60
+
+onready var __bomb_timer = $BombTimer
+
+onready var __sprite  = $AnimatedSprite
+
+onready var __body_shape = $StaticBody2D/CollisionShape2D
+onready var __press_shape = $PressArea/CollisionShape2D
+
+var __count_down = false
+
+func _physics_process(delta):
+	if __count_down:
+		__parent_level.game_handler.get_hud().set_global_message("ESCAPE! - " + str(round(__bomb_timer.time_left)))
+		
+func _on_PressArea_body_entered(body):
+	__bomb_timer.start(__detonation_time)
+	__count_down = true
+	__sprite.animation = "on"
+	__body_shape.set_deferred("disabled", true)
+	__press_shape.set_deferred("disabled", true)
+	
+func is_ticking():
+	return __count_down
+
+# TODO: Show explosion and die
+func _on_BombTimer_timeout():
+	pass # Replace with function body.
