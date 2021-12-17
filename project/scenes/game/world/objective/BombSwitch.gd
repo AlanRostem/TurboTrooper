6a3e45b748a4670665fe4f984bd6a915298e5038
Alan Rostem <arostem@gmail.com>
2021-12-17 18:25:18 +0100

    stand

--- project/scenes/game/world/objective/BombSwitch.gd
+++ project/scenes/game/world/objective/BombSwitch.gd
@@ -15,19 +15,23 @@ onready var __press_shape = $PressArea/CollisionShape2D
 
 var __count_down = false
 
+var __player_on_top = false
+
 func _physics_process(delta):
 	if __count_down:
 		__parent_level.game_handler.get_hud().set_global_message("ESCAPE! - " + str(round(__bomb_timer.time_left)))
+	elif __player_on_top:
+		__bomb_timer.start(__detonation_time)
+		__count_down = true
+		__sprite.animation = "on"
+		__body_shape.set_deferred("disabled", true)
+		__press_shape.set_deferred("disabled", true)
+		__arrow_sprite.visible = false
+		emit_signal("activated")
+		get_parent_level().save_check_point()
 		
 func _on_PressArea_body_entered(body):
-	__bomb_timer.start(__detonation_time)
-	__count_down = true
-	__sprite.animation = "on"
-	__body_shape.set_deferred("disabled", true)
-	__press_shape.set_deferred("disabled", true)
-	__arrow_sprite.visible = false
-	emit_signal("activated")
-	get_parent_level().save_check_point()
+	__player_on_top = true
 	
 func is_ticking():
 	return __count_down
@@ -43,6 +47,8 @@ func _on_BombTimer_timeout():
 	__parent_level.game_handler.get_hud().hide_global_message()
 	__count_down = false
 
-
 func _on_BombSwitch_completed():
 	stop_ticking()
+
+func _on_PressArea_body_exited(body):
+	__player_on_top = false
