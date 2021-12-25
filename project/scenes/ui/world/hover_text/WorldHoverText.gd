ba935595048f4cae38ea27df73aa426beb36d735
Alan Rostem <arostem@gmail.com>
2021-12-25 17:03:09 +0100

    hmm

--- project/scenes/ui/world/hover_text/WorldHoverText.gd
+++ project/scenes/ui/world/hover_text/WorldHoverText.gd
@@ -7,7 +7,8 @@ const HOVER_SPEED = 20
 onready var __life_timer = $LifeTimer
 
 func display(str_txt, location):
-	text = str_txt	
+	rect_size.x = 0
+	text = str_txt
 	rect_position = location - rect_size / 2
 	visible = true
 	__life_timer.start()
