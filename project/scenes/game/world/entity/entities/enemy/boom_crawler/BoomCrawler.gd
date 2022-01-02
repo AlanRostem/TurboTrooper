c3e9201249b9906b8921c7fb4b7c262f1f3e4a80
Alan Rostem <arostem@gmail.com>
2022-01-02 15:55:40 +0100

    making level for tweet

--- project/scenes/game/world/entity/entities/enemy/boom_crawler/BoomCrawler.gd
+++ project/scenes/game/world/entity/entities/enemy/boom_crawler/BoomCrawler.gd
@@ -4,16 +4,18 @@ var __explosion_effect_scene = preload("res://scenes/game/world/other/BoomCrawle
 
 const WALK_SPEED = 30
 
-export var __direction = -1
-
-func _ready():
-	set_velocity_x(__direction * WALK_SPEED)
+var __direction = -1
 
 func _physics_process(delta):
 	if is_on_wall():
-		set_velocity_x(-get_velocity().x)
+		__direction *= -1
+	set_velocity_x(__direction * WALK_SPEED)
 
 
 func _on_OutHitBox_hit_dealt_then_destroy_self(hitbox):
 	queue_free()
 	parent_world.show_effect_deferred(__explosion_effect_scene, position)
+
+
+func _on_BoomCrawler_player_detected(player):
+	__direction = get_horizontal_player_detect_direction()
