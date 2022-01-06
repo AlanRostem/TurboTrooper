0fcbb6c94c84a19ac3cbb0299b00c507528e9f78
Alan Rostem <arostem@gmail.com>
2022-01-06 01:33:38 +0100

    working on new hazard

---
+++ project/scenes/game/world/environment/hazards/MovingSpikeHazard.gd
@@ -0,0 +1,43 @@
+extends MovingEntity
+
+onready var __out_hit_box_shape = $OutHitBox/CollisionShape2D
+onready var __body_shape = $BodyShape
+onready var __disabled_timer = $DisabledTimer
+onready var __sprite = $AnimatedSprite
+
+const ANGLE_SPEED = PI * 2.0 / 3.0
+const SPEED = 35
+
+var __enabled = true
+var __angle = 0
+var __target_pos:Vector2
+
+func _physics_process(delta):
+	if !__enabled:
+		position = __target_pos
+		return
+	__angle += ANGLE_SPEED * delta
+	set_velocity_y(sin(__angle) * SPEED)
+	position.y += get_velocity().y * delta
+
+func set_enabled(value):
+	__body_shape.set_deferred("disabled", value)
+	__out_hit_box_shape.set_deferred("disabled", !value)
+	__enabled = value
+	__sprite.animation = "default" if value else "disabled"
+
+func _on_InHitBox_hit_received(hitbox, damage, damage_type):
+	if damage_type == HealthComponent.DAMAGE_TYPE_ENERGY or damage_type == HealthComponent.DAMAGE_TYPE_CRITICAL:
+		set_enabled(false)
+		__disabled_timer.start()
+		set_velocity_y(0)
+		__target_pos = position
+
+
+func _on_DisabledTimer_timeout():
+	set_enabled(true)
+	
+func _on_OutHitBox_hit_dealt(hitbox):
+	var player = hitbox.get_parent()
+	if player is Player:
+		player.stats.take_one_damage()
