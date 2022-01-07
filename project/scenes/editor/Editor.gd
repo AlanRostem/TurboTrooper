0517ac44de4509218349a1b3954f55e72034a12a
Alan Rostem <arostem@gmail.com>
2022-01-07 17:25:09 +0100

    editor changed to fit filedialog

--- project/scenes/editor/Editor.gd
+++ project/scenes/editor/Editor.gd
@@ -5,40 +5,15 @@ onready var __canvas = $Canvas
 onready var __tiles = $MenuLayer/Tiles
 onready var __entities = $MenuLayer/Entities
 onready var __tools = $MenuLayer/Tools
-onready var __tile_tween = $MenuLayer/Tiles/TileTween
-onready var __entities_tween = $MenuLayer/Entities/EntitiesTween
-onready var __tools_tween = $MenuLayer/Tools/ToolsTween
 onready var __camera = $Canvas/CameraHolder/Camera2D
-onready var __file_dialog = $FileDialog
+onready var __file_dialog = $MenuLayer/FileDialog
 
 
 func get_editor_canvas():
 	return __canvas
 
-func _on_TilesButton_toggled(button_pressed):
-	if button_pressed:
-		__tile_tween.interpolate_property(__tiles, "rect_position:x", __tiles.rect_position.x, 0, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	else:
-		__tile_tween.interpolate_property(__tiles, "rect_position:x", __tiles.rect_position.x, -__tiles.rect_size.x, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	__tile_tween.start()
-
-func _on_EntitiesButton_toggled(button_pressed):
-	if button_pressed:
-		__entities_tween.interpolate_property(__entities, "rect_position:x", __entities.rect_position.x, rect_size.x - __entities.rect_size.x, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	else:
-		__entities_tween.interpolate_property(__entities, "rect_position:x", __entities.rect_position.x, rect_size.x, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	__entities_tween.start()
-
-func _on_ToolsButton_toggled(button_pressed):
-	if button_pressed:
-		__tools_tween.interpolate_property(__tools, "rect_position:y", __tools.rect_position.y, 0, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	else:
-		__tools_tween.interpolate_property(__tools, "rect_position:y", __tools.rect_position.y, -__tools.rect_size.y, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	__tools_tween.start()
-
 func _on_PauseButton_pressed():
 	get_tree().paused = !get_tree().paused
-
 
 func _on_SaveButton_pressed():
 	__file_dialog.popup()
