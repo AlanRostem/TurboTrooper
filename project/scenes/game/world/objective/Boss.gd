3a46a76b4c2c27e4d958f5a2f47e64e2010c05fe
Alan Rostem <arostem@gmail.com>
2022-01-17 22:20:58 +0100

    fix

--- project/scenes/game/world/objective/Boss.gd
+++ project/scenes/game/world/objective/Boss.gd
@@ -22,11 +22,11 @@ func _on_HealthComponent_health_depleted(health_left):
 func _on_RocketRogueSpawnTimer_timeout():
 	if __spawn_left:
 		__spawn_left = false
-		var rogue = get_parent_level().get_game_world().spawn_entity(__rocket_rogue_scene, __spawn_pos_left.global_position)
+		var rogue = get_parent_level().get_game_world().spawn_entity(__rocket_rogue_scene, position + __spawn_pos_left.position)
 		rogue.horizontal_looking_direction = 1
 	else:
 		__spawn_left = true
-		var rogue = get_parent_level().get_game_world().spawn_entity(__rocket_rogue_scene, __spawn_pos_right.global_position)
+		var rogue = get_parent_level().get_game_world().spawn_entity(__rocket_rogue_scene, position + __spawn_pos_right.position)
 		rogue.horizontal_looking_direction = -1
 
 func _on_HitBox_hit_received(hitbox, damage, damage_type):
