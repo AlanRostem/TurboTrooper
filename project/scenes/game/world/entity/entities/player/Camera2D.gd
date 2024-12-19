8c43d7ac30c98a4f05ca9ddc1ea40fad55081533
AlanRostem <arostem@gmail.com>
2024-12-19 15:06:12 +0100

    camera much smoother now

--- project/scenes/game/world/entity/entities/player/Camera2D.gd
+++ project/scenes/game/world/entity/entities/player/Camera2D.gd
@@ -16,7 +16,8 @@ var __limit_y
 
 const WIDTH = 160
 const HEIGHT = 144
-const SPEED = 3
+const SPEED_UP = 1
+const SPEED_DOWN = 3
 
 func set_height_index_by_ypos(ypos):
 	__height_index = int(ypos / HEIGHT)
@@ -38,39 +39,38 @@ func set_bounds(bounds: Rect2, player_pos: Vector2):
 func _physics_process(delta):
 	var drag_x = 0.1
 	var margin_x = drag_x * WIDTH
-	var drag_y = 1.0
-	var margin_y = drag_y * HEIGHT
+	var drag_max_y = 0.2
+	var drag_min_y = 0.2
 
 	if __follow_node.position.x > (position.x + margin_x): # Right
 		position.x = (__follow_node.position.x - margin_x)
 	elif __follow_node.position.x < (position.x - margin_x): # Left
 		position.x = (__follow_node.position.x + margin_x)
 	
-	if __follow_node.position.y > (position.y + margin_y): # Bottom
-		position.y = (__follow_node.position.y - margin_y)
-	elif __follow_node.position.y < (position.y - margin_y): # Top
-		position.y = (__follow_node.position.y + margin_y)
+	if __follow_node.position.y > (position.y + drag_max_y * HEIGHT): # Bottom
+		position.y = (__follow_node.position.y - drag_max_y * HEIGHT)
+	elif __follow_node.position.y < (position.y - drag_min_y * HEIGHT): # Top
+		position.y = (__follow_node.position.y + drag_min_y * HEIGHT)
 		
 	position.x = clamp(position.x, WIDTH/2, __limit_x-WIDTH/2)
 #	position.y = clamp(position.y, HEIGHT/2, __limit_y-HEIGHT/2)
-	position.y = __limit_y - HEIGHT/2
+	position.y = clamp(position.y, HEIGHT/2, __limit_y-HEIGHT/2)
 		
-	var desired = (__height_index+1) * HEIGHT
-	if __transition_direction != 0:
-		__limit_y += sign(__transition_direction) * SPEED
-		if __transition_direction < 0 and __limit_y < desired:
-			__limit_y = desired
-		if __transition_direction > 0 and __limit_y > desired:
-			__limit_y = desired
-		if __limit_y == desired:
-			get_tree().paused = false
-			__transition_direction = 0
-			print("done")
-		return
-	
 	var live_height_index = int(__follow_node.position.y / HEIGHT)
-	if __height_index != live_height_index:
-		__transition_direction = live_height_index - __height_index
-		print("change:", __height_index, "->", live_height_index)
-		__height_index = live_height_index
-		get_tree().paused = true
+	var camera_height_index = int(position.y / HEIGHT)
+	var desired = (live_height_index+1) * HEIGHT
+	var speed = SPEED_UP if __transition_direction < 0 else SPEED_DOWN
+	if live_height_index != __height_index:
+		var dir = live_height_index - __height_index
+		__update_height_transition(dir, desired)
+	elif camera_height_index != live_height_index:
+		var dir = live_height_index - camera_height_index
+		__update_height_transition(dir, desired)
+
+func __update_height_transition(dir, desired):
+	var speed = SPEED_UP if dir < 0 else SPEED_DOWN
+	__limit_y += sign(dir) * speed
+	if dir < 0 and __limit_y < desired:
+		__limit_y = desired
+	if dir > 0 and __limit_y > desired:
+		__limit_y = desired
